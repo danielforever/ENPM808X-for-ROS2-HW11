4212c579d0514fa6b235fd959d6cd7396f2bcf94
danielforever <daniel87011354@gmail.com>
2022-12-05 21:42:03 -0500

    add listener.cpp and talker.cpp

---
+++ src/listener.cpp
@@ -0,0 +1,12 @@
+// Copyright 2022 Po-Yu Huang
+#include "my_package/subscriber.hpp"
+
+int main(int argc, char* argv[]) {
+  rclcpp::init(argc, argv);
+  RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"),
+              "\nFive color: \n   1. Yellow  2. Green  3. Blue  4. Orange  5. Red \n");
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+
+  rclcpp::shutdown();
+  return 0;
+}
