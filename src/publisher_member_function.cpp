82d8bb5dc0b8332aef849c7cd446e735bc99337a
danielforever <daniel87011354@gmail.com>
2022-12-07 22:08:21 -0500

    After cpp check

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,38 +1,27 @@
 // Copyright 2022 Po-Yu Huang
 #include "my_package/publisher.hpp"
 
-MinimalPublisher::MinimalPublisher()
-: Node("minimal_publisher"), count_(0)
-{
+MinimalPublisher::MinimalPublisher() : Node("minimal_publisher"), count_(0) {
 
-  
-  auto count_number =
-    rcl_interfaces::msg::ParameterDescriptor{};
-
-  count_number.description = "\nThis could pass the count\n";
-
-  this->declare_parameter("counting", count_, count_number);
+  this->declare_parameter("counting", count_, rcl_interfaces::msg::ParameterDescriptor{});
 
   count_ = this->get_parameter("counting").get_parameter_value().get<int>();
 
-
   publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
   timer_ = this->create_wall_timer(
-    1s, std::bind(&MinimalPublisher::timer_callback, this));
+      1s, std::bind(&MinimalPublisher::timer_callback, this));
 
   get_count_service_ = this->create_service<my_package::srv::CountNumber>(
-    "CountNumber",
-    std::bind(&MinimalPublisher::number_callback, this, _1, _2));
+      "CountNumber",
+      std::bind(&MinimalPublisher::number_callback, this, _1, _2));
 
   tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 
   tf_timer_ = this->create_wall_timer(
-    300ms, std::bind(&MinimalPublisher::tf_callback, this));
-
+      300ms, std::bind(&MinimalPublisher::tf_callback, this));
 }
 
-void MinimalPublisher::timer_callback()
-{
+void MinimalPublisher::timer_callback() {
   auto message = std_msgs::msg::String();
 
   count_ = this->get_parameter("counting").get_parameter_value().get<int>();
@@ -45,16 +34,13 @@ void MinimalPublisher::timer_callback()
 }
 
 void MinimalPublisher::number_callback(
-  const std::shared_ptr<my_package::srv::CountNumber::Request> request,
-  std::shared_ptr<my_package::srv::CountNumber::Response> response)
-{
-  (void) request;
+    const std::shared_ptr<my_package::srv::CountNumber::Request> request,
+    std::shared_ptr<my_package::srv::CountNumber::Response> response) {
+  (void)request;
   response->sum = count_;
 }
 
-void MinimalPublisher::tf_callback()
-{
-
+void MinimalPublisher::tf_callback() {
   geometry_msgs::msg::TransformStamped t;
 
   t.header.stamp = this->get_clock()->now();
@@ -62,6 +48,7 @@ void MinimalPublisher::tf_callback()
   t.child_frame_id = "talk";
 
   tf2::Quaternion q;
+
   q.setRPY(0, 0, 1);
 
   t.transform = tf_talker;
@@ -74,4 +61,3 @@ void MinimalPublisher::tf_callback()
   tf_talker.rotation.w = q.w();
   tf_broadcaster_->sendTransform(t);
 }
-
